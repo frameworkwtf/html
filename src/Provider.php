5791f665cff9ce15db876a82a1158b9411a0a446
Nikita Chernyi <developer.nikus@gmail.com>
2018-03-28 11:28:40 +0300

    updated codestyle

--- src/Provider.php
+++ src/Provider.php
@@ -39,7 +39,7 @@ class Provider implements ServiceProviderInterface
             ]);
 
             // Instantiate and add Slim specific extension
-            $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+            $basePath = \rtrim(\str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
             $view->addExtension(new \Slim\Views\TwigExtension($container['router'], $basePath));
             $view->addExtension(new \Knlv\Slim\Views\TwigMessages($container['flash']));
             $view->addExtension(new \Wtf\Html\CsrfExtension($container['csrf_middleware']));
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-session_start();
+\session_start();
 // Enable Composer autoloader
 /** @var \Composer\Autoload\ClassLoader $autoloader */
 $autoloader = require __DIR__.'/../vendor/autoload.php';
